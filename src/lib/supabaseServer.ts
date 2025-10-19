2823246ad7f3c254af2b7f62b01b151c2da00b51
Abdulmuiz44 <abdulmuizproject@gmail.com>
2025-10-19 07:24:54 +0100

    ADDED SUPABASE URL AND ANON KEY

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseUrl = 'https://xbmcovqzgcukluprfgnz.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhibWNvdnF6Z2N1a2x1cHJmZ256Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3MzA0ODMsImV4cCI6MjA3NjMwNjQ4M30.uWo_UEbUbZdxqUulygBvrAshqdzfQE0OZ0pgAHC4'
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase URL or Key for server-side')
